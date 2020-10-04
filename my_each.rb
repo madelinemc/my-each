13617067b6b41c8dbfb14d137e2c37afda7bd192
madelinemc <madeline.coons@gmail.com>
2020-10-04 18:30:25 -0500

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,15 @@
-def my_each # put argument(s) here
-  # code here
+#array = [1, 2, 3, 4]
+#method meant to 
+
+def my_each(array) 
+  if block_given?
+    variable = 0
+    while variable < array.length
+      yield(array[variable])
+      variable += 1
+    end
+    array
+  else 
+      "You forgot to input numbers!"
+  end
 end
